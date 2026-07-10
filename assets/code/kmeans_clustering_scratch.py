098ecca151c5b9508c086efdf3c957447e1bb35f
Gawon Kim <gawonkim5@gmail.com>
2026-07-10 11:01:13 +0900

    Strip instructor-style docstrings from K-means code

--- assets/code/kmeans_clustering_scratch.py
+++ assets/code/kmeans_clustering_scratch.py
@@ -14,23 +14,6 @@ K = initial_centroids.shape[0]
 X.shape[0]
 
 def find_closest_centroid(X, centroids):
-
-    """
-    Returns the closest centroids in idx for a dataset X
-    where each row is a single example. idx = m x 1 vector
-    (i.e. each entry in range [1..K])
-    Args:
-        X        : array(# examples, n)
-        centroids: array(K, n)
-    Returns:
-        idx      : array(# examples, 1)
-
-    Iterate over every example, find its closest centroid, and store
-    the index inside idx at the appropriate location. Concretely,
-    idx[i] should contain the index of the centroid closest to
-    example i. Hence, it should be a value in the range 1..K.
-    """
-
     idx = np.zeros((X.shape[0],1))
     for i in range(X.shape[0]):
         idx[i] = np.argmin(np.sqrt(np.sum((X[i,:]-centroids)**2, axis=1)))+1
@@ -40,22 +23,6 @@ def find_closest_centroid(X, centroids):
 idx = find_closest_centroid(X, initial_centroids)
 
 def compute_centroids(X, idx, K):
-    """
-    Returns the new centroids by computing the means
-    of the data points assigned to each centroid. It is
-    given a dataset X where each row is a single data point,
-    a vector idx of centroid assignments (i.e. each entry
-    in range [1..K]) for each example, and K, the number of
-    centroids. A matrix centroids is returned, where each row
-    of centroids is the mean of the data points assigned to it.
-    Args:
-        X        : array(# training examples, 2)
-        idx      : array(# training examples, 1)
-        K        : int, # of centroids
-    Returns:
-        centroids: array(# of centroids, 2)
-    """
-
     centroids = np.zeros((K, X.shape[1]))
     for i in range(K):
         centroids[i,:] = np.mean(X[(idx==i+1).T[0], :], axis=0)
@@ -72,30 +39,6 @@ def plot_kmeans(X, centroids, previous, idx, K, axes):
 
 
 def run_kmeans(X, initial_centroids, max_iters, plot_progress):
-    """
-    Runs the K-Means algorithm on data matrix X, where each row of X
-    is a single example. It uses initial_centroids used as the initial
-    centroids. max_iters specifies the total number of interactions
-    of K-Means to execute. plot_progress is a true/false flag that
-    indicates if the function should also plot its progress as the
-    learning happens.
-
-    run_kmeans returns
-    - centroids,
-    - K x n matrix of the computed centroids and idx,
-    - m x 1 vector of centroid assignments (i.e. each entry in range [1..K])
-
-    Args:
-        X                : array(# training examples, n)
-        initial_centroids: array(# of centroids, n)
-        max_iters        : int, # of iterations
-        plot_progress    : boolean, default set to False
-    Returns:
-        centroids        : array(# of centroids, n)
-        idx              : array(# training examples, 1)
-    """
-
-
     if plot_progress:
         ncols = 3
         nrows = int(max_iters/ncols)
@@ -137,17 +80,6 @@ centroids, idx = run_kmeans(X, initial_centroids, max_iters, plot_progress=True)
 # random initialization
 
 def init_random_centroids(X, K):
-    """
-    Initializes K centroids that are to be
-    used in K-Means on the dataset X.
-
-    Args:
-        X                : array(# training examples, n)
-        K                : int, # of centroids
-    Returns:
-        initial_centroids: array(# of centroids, n)
-
-    """
     centroids = X[(np.random.choice(X.shape[0], K)), :]
     return centroids
 
